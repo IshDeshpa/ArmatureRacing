3f8b153c18a69666f128ae8922eb5d77450315b4
ISHAN-LAPTOP\IshDeshpa <ishdeshpa@gmail.com>
2021-05-19 19:50:39 -0500

    added latest VR code for build bash

---
+++ HDHexMotorTest/HDHexMotorTest.ino
@@ -0,0 +1,36 @@
+#include <Servo.h>
+
+int pwmPin = 2;
+int powerPin = 26;
+
+int pulseWidthUs = 2000;
+int pdMs =  1/50 * 1000;
+
+int pulseWidthMax = 2000;
+int pulseWidthNeutral = 1500;
+int pulseWidthMin = 1000;
+
+Servo motor;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(powerPin, OUTPUT);
+  //pinMode(pwmPin, OUTPUT);
+
+  motor.attach(pwmPin);
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(powerPin, HIGH);
+  //analogWrite(pwmPin, (pulseWidthUs - pulseWidthNeutral + 40)/(pulseWidthMax - pulseWidthNeutral + 40) * 255);
+  //Serial.println((pulseWidthUs - pulseWidthNeutral + 40)/(pulseWidthMax - pulseWidthNeutral + 40) * 255);
+//  digitalWrite(pwmPin, HIGH);
+//  delay(pulseWidthMs);
+//  digitalWrite(pwmPin, LOW);
+//  delay(pdMs - pulseWidthMs);
+  motor.writeMicroseconds(2000);
+}
---
+++ VRSimulator/VRJoystickRacecar/VRJoystickRacecar.ino
@@ -0,0 +1,109 @@
+#include "Joystick.h"
+#include "RotaryEncoder.h"
+
+/*Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
+  JOYSTICK_TYPE_JOYSTICK, 0, 0,
+  true, false, false, false, false, false,
+  false, false, false, false, false);*/
+
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
+  JOYSTICK_TYPE_MULTI_AXIS, 0, 0,
+  true, true, false, false, false, false,
+  false, false, true, true, false);
+
+const int powerPin1 = 22;
+const int powerPin2 = 24;
+
+const int accelPedalPin = A0;
+const int minValueAccel = 255;
+const int maxValueAccel = 700;
+int accelerator = 0;
+
+const int brakePedalPin = A1;
+const int minValueBrake = 170;
+const int maxValueBrake = 310;
+int brake = 0;
+
+const int steeringOutputA = 48;
+const int steeringOutputB = 50;
+const int encoderRes = 300;
+
+//Force Feedback
+Gains mygains[2];
+EffectParams myeffectparams[2];
+int32_t forces[2] = {0};
+
+RotaryEncoder encoder(steeringOutputA, steeringOutputB, RotaryEncoder::LatchMode::TWO03);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  Joystick.setAcceleratorRange(0, maxValueAccel - minValueAccel);
+  Joystick.setBrakeRange(0, maxValueBrake - minValueBrake);
+  Joystick.setXAxisRange(0, encoderRes/2*6);
+
+  mygains[0].totalGain = 100;//0-100
+  mygains[0].springGain = 100;//0-100
+
+  pinMode(powerPin1, OUTPUT);
+  pinMode(powerPin2, OUTPUT);
+
+  pinMode(steeringOutputA, INPUT);
+  pinMode(steeringOutputB, INPUT);
+
+  encoder.setPosition(0);
+
+  Joystick.setGains(mygains);
+  Joystick.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(powerPin1, HIGH);
+  digitalWrite(powerPin2, HIGH);
+  
+  accelerator = analogRead(accelPedalPin);
+  if(accelerator < minValueAccel){
+    accelerator = minValueAccel;
+  }
+  else if(accelerator > maxValueAccel){
+    accelerator = maxValueAccel;  
+  }
+  accelerator -= minValueAccel;
+
+  Joystick.setAccelerator(accelerator);
+
+  brake = analogRead(brakePedalPin);
+  
+  if(brake < minValueBrake){
+    brake = minValueBrake;
+  }
+  else if(accelerator > maxValueBrake){
+    brake = maxValueBrake;  
+  }
+  brake -= minValueBrake;
+
+  Joystick.setBrake(brake);
+
+  static int pos = 0;
+  encoder.tick();
+
+  int newPos = encoder.getPosition();
+  if (pos != newPos) {
+    //Serial.println(newPos);
+    pos = newPos;
+  }
+
+  myeffectparams[0].springMaxPosition = encoderRes/2*6;
+  myeffectparams[0].springPosition = newPos;
+
+  Joystick.setXAxis(newPos);
+
+  Joystick.setEffectParams(myeffectparams);
+  Joystick.getForce(forces);
+  
+  
+  
+  delay(1);
+}
